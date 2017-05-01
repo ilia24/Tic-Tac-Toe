bbdba808092c84a0e509c290b1d1fd515e784361
ilia24 <iliademertchian24@gmail.com>
2017-05-01 18:59:38 -0400

    Add horrifyingly unrefactored IF/ELSE game logic

--- js/main.js
+++ js/main.js
@@ -14,6 +14,47 @@ $(function() {
       $('#gamegrid').attr('class', 'turn_x')
     }
 
+
+    if ($('.block.one p').attr('class') === 'x' && $('.block.two p').attr('class') === 'x' && $('.block.three p').attr('class') === 'x') {
+      console.log('X wins the game');
+    } else if ($('.block.one p').attr('class') === 'o' && $('.block.two p').attr('class') === 'o' && $('.block.three p').attr('class') === 'o') {
+      console.log('O wins the game');
+    } else if ($('.block.four p').attr('class') === 'x' && $('.block.five p').attr('class') === 'x' && $('.block.six p').attr('class') === 'x') {
+      console.log('X wins the game');
+    } else if ($('.block.four p').attr('class') === 'o' && $('.block.five p').attr('class') === 'o' && $('.block.six p').attr('class') === 'o') {
+      console.log('O wins the game');
+    } else if ($('.block.seven p').attr('class') === 'x' && $('.block.eight p').attr('class') === 'x' && $('.block.nine p').attr('class') === 'x') {
+      console.log('X wins the game');
+    } else if ($('.block.seven p').attr('class') === 'o' && $('.block.eight p').attr('class') === 'o' && $('.block.nine p').attr('class') === 'o') {
+      console.log('O wins the game'); // LEFT TO RIGHT ENDS HERE, TOP TO BOTTOM BEGINS HERE
+    } else if ($('.block.one p').attr('class') === 'x' && $('.block.four p').attr('class') === 'x' && $('.block.seven p').attr('class') === 'x') {
+      console.log('X wins the game');
+    } else if ($('.block.one p').attr('class') === 'o' && $('.block.four p').attr('class') === 'o' && $('.block.seven p').attr('class') === 'o') {
+      console.log('O wins the game');
+    } else if ($('.block.two p').attr('class') === 'x' && $('.block.five p').attr('class') === 'x' && $('.block.eight p').attr('class') === 'x') {
+      console.log('X wins the game');
+    } else if ($('.block.two p').attr('class') === 'o' && $('.block.five p').attr('class') === 'o' && $('.block.eight p').attr('class') === 'o') {
+      console.log('O wins the game');
+    } else if ($('.block.three p').attr('class') === 'x' && $('.block.six p').attr('class') === 'x' && $('.block.nine p').attr('class') === 'x') {
+      console.log('X wins the game');
+    } else if ($('.block.three p').attr('class') === 'o' && $('.block.six p').attr('class') === 'o' && $('.block.nine p').attr('class') === 'o') {
+      console.log('O wins the game'); //TOP DOWN ENDS HERE
+    } else if ($('.block.one p').attr('class') === 'o' && $('.block.five p').attr('class') === 'o' && $('.block.nine p').attr('class') === 'o') {
+      console.log('O wins the game');
+    } else if ($('.block.one p').attr('class') === 'x' && $('.block.five p').attr('class') === 'x' && $('.block.nine p').attr('class') === 'x') {
+      console.log('X wins the game');
+    } else if ($('.block.three p').attr('class') === 'o' && $('.block.five p').attr('class') === 'o' && $('.block.seven p').attr('class') === 'o') {
+      console.log('O wins the game');
+    } else if ($('.block.three p').attr('class') === 'x' && $('.block.five p').attr('class') === 'x' && $('.block.seven p').attr('class') === 'x') {
+      console.log('X wins the game');
+    }
+
+
+
+
+
+
+
   });
 });
 
